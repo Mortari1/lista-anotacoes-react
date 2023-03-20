8b72fc3af37058d693da6720bae7d8d325bb5ee6
Mortari1 <henriquemortari1@gmail.com>
2023-03-20 08:20:20 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,127 +1,128 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
- 
 
-  const [tarefas,setTarefas] = useState([
+  const [tarefas, setarTarefas] = useState([
     /*
     {
       id:0,
-      tarefa:'Fazer molho branco pro macarrão',
-      finalizada: false
+      tarefa:'Minha tarefa do dia',
+      finalizada:false
     },
     {
-      id:1,
-      tarefa:'Ir ao mercado',
-      finalizada: true
-    }*/
+      id:0,
+      tarefa:'Minha tarefa do dia 2',
+      finalizada:true
+    },
+    */
+
+
   ]);
-
-  const salvarTarefa = () =>{
-    //Todo: Salvar a tarefa.
-    var tarefa = document.getElementById("content-tarefa");
-    
-    setTarefas([
-      ...tarefas,
-      {
-        id: new Date().getTime(),
-        tarefa: tarefa.value,
-        finalizada:false
-      }
-    ]);
-
-    
-    window.localStorage.setItem('tarefas',JSON.stringify([
-      ...tarefas,
-      {
-        id: new Date().getTime(),
-        tarefa: tarefa.value,
-        finalizada:false
-      }
-    ]));
-    setModal(false);
-  }
-
-  const marcarConcluida = (id,opt)=>{
-    let novasTarefas = tarefas.filter((val)=>{
-     
-      if(val.id === id){
-        val.finalizada = opt;
-        
-      }
-      
-      return val;
-    })
-
-    setTarefas(novasTarefas);
-    window.localStorage.setItem('tarefas',JSON.stringify(novasTarefas));
-  }
-
-  
   const [modal, setModal] = useState(false);
-  //abre a modal para adicionar tarefas
-  const abrirModal = () => {
-    setModal(!modal);
+
+
+  const salvarTarefa = () => {
+      //TODO: Salvar a tarefa.
+      var tarefa = document.getElementById('content-tarefa');
+      
+      setarTarefas([
+        ...tarefas,
+        {
+          id: new Date().getTime(),
+          tarefa: tarefa.value,
+          finalizada:false
+
+        }
+      ]);
+
+      window.localStorage.setItem('tarefas',JSON.stringify([
+        ...tarefas,
+        {
+          id: new Date().getTime(),
+          tarefa: tarefa.value,
+          finalizada:false
+
+        }
+      ]));
+
+      setModal(false);
+  
   }
 
-  const deletarTarefa = (id) =>{
-   let pegarTarefas = tarefas.filter((val)=>{
-    if(val.id !== id){
-      
-      return val;
-    }
-   })
+  const marcarConcluida = (id,opt) =>{
+        let novasTarefas = tarefas.filter(function(val){
+                if(val.id == id){
+                  val.finalizada = opt;
+                }
 
-   setTarefas(pegarTarefas);
+                return val;
+        })
+
+        setarTarefas(novasTarefas);
+        window.localStorage.setItem('tarefas',JSON.stringify(novasTarefas));
+  }
+
+  const abrirModal = () => {
+        setModal(!modal);
+  }
+
+  const deletarTarefa = (id)=>{
+    
+    let novasTarefas = tarefas.filter(function(val){
+        if(val.id != id){
+          return val;
+      }
+    })
+    
+    setarTarefas(novasTarefas);
   }
 
   useEffect(()=>{
-    //Fazer uma chamada para API e preencher tarefas
-    if(window.localStorage.getItem('tarefas') !== undefined){
-      setTarefas(JSON.parse(window.localStorage.getItem('tarefas')));
-    }
-    
-  },[]);
+      
+      if(window.localStorage.getItem('tarefas') != undefined){
+          setarTarefas(JSON.parse(window.localStorage.getItem('tarefas')));
+            console.log(window.localStorage.getItem('tarefas'));
+      }
+  },[])
+
   return (
     <div className="App">
-      {
-        modal?
-        <div className='modal'>
-          <div className='modalContent'>
-            <h3>Adicionar sua tarefa</h3>
-            <input id="content-tarefa" type="text"/>
-            <button onClick={()=>salvarTarefa()}>Salvar</button>
+          {
+            modal?
+            <div className="modal">
+                <div className="modalContent">
+                    <h3>Adicionar sua tarefa</h3>
+                    <input id="content-tarefa" type="text" />
+                    <button onClick={()=>salvarTarefa()}>Salvar!</button>
+                </div>
+            </div>
+            :
+            <div></div>
+          }
+          <div onClick={()=>abrirModal()} className="addTarefa">+</div>
+          <div className="boxTarefas">
+              <h2>Minha Tarefas do Dia!</h2>
+              {
+                tarefas.map((val)=>{
+                    if(!val.finalizada){
+                      return (
+                          <div className="tarefaSingle">
+                          <p onClick={()=>marcarConcluida(val.id,true)}>{val.tarefa}</p>
+                          <span onClick={()=>deletarTarefa(val.id)} style={{color:'red'}}>(x)</span>
+                          </div>
+                      );
+                    }else{
+                      return (
+                        <div className="tarefaSingle">
+                        <p onClick={()=>marcarConcluida(val.id,false)} style={{textDecoration:'line-through'}}>{val.tarefa}</p>
+                        <span onClick={()=>deletarTarefa(val.id)} style={{color:'red'}}>(x)</span>
+                        </div>
+                      );
+                    }
+                })
+              }
           </div>
-        </div>
-        :
-        <div>
-          
-        </div>
-      }
-      <div onClick={()=>abrirModal()} className='addTarefa'></div>
-      <div className='boxTarefas'>
-        <h2>Minhas Tarefas do Dia!</h2>
-        {
-          tarefas.map((val)=>{
-            if(!val.finalizada){
-              return(
-                <div className='tarefaSingle'>
-                  <p onClick={()=>marcarConcluida(val.id,true)}>{val.tarefa}</p>
-                  <span onClick={()=>deletarTarefa(val.id)}>(X)</span>
-                </div>
-              );
-            }else{
-              return(
-                <div className='tarefaSingle'>
-                  <p onClick={()=>marcarConcluida(val.id,false)} style={{textDecoration:'line-through'}}>{val.tarefa}</p>
-                  <span onClick={()=>deletarTarefa(val.id)}>(X)</span>
-                </div>
-              );
-            }
-          })
-        }
-      </div>
     </div>
   );
 }
